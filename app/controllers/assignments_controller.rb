28b6167907e705a2187dc935457510545cba3eb5
sunny19 <sunny@99tests.com>
2016-06-11 17:29:50 +0530

    fix

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,12 +1,16 @@
 class AssignmentsController < ApplicationController
   def new
-    array = eval(params[:array])
-    @final_array = Assignment.assign(array)
-    if @final_array[1] > 0
-      @final_value = "Array is not good"
+    if params[:array].kind_of?(Array)
+      array = eval(params[:array])
+      @final_array = Assignment.assign(array)
+      if @final_array[1] > 0
+        @final_value = "Array is not good"
+      else
+        @final_value = @final_array[0]
+      end
     else
-      @final_value = @final_array[0]
-    end 
+      @final_value = "Please use proper format of array"
+    end
     respond_to do |format|
       format.js
     end
